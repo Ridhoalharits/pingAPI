8296a882c7d156aad01e38eb7e7e73176ae0f89c
Ridho Al Harits <ridhoalharits@gmail.com>
2025-04-26 11:59:20 +0700

    add new endpoint and dockerfile conf

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
 
+app.get("/hello", (req, res) => {
+  res.status(200).send("Motherfucker");
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
