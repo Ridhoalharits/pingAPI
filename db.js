2f2ad0bf52480283904d3a01356ae772a7e4cfb4
Ridho Al Harits <ridhoalharits@gmail.com>
2025-04-30 21:37:12 +0700

    fix

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_DATABASE || "mydb",
-  password: process.env.DB_PASSWORD || "dhior",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
 });
 
